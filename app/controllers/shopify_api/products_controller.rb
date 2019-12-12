2e08db130723bf12358771f6be4812688761c8dd
Tripti <tripti.bhangade@gemsessence.com>
2019-12-12 16:37:03 +0530

    temp

--- app/controllers/shopify_api/products_controller.rb
+++ app/controllers/shopify_api/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class ShopifyApi::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+	namespace :shopify_api do
+  	resources :products
+	end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 end
